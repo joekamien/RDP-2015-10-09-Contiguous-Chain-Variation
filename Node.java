1046683d9bec2531925bca18140ed8509e94289e
joekamien <joekamien@users.noreply.github.com>
2015-10-09 12:13:06 -0700

    Fix warnings in Node.java

--- Node.java
+++ Node.java
@@ -1,7 +1,7 @@
 import java.util.ArrayList;
 
 public class Node {
-	private ArrayList<Edge> edges = new ArrayList(4);
+	private ArrayList<Edge> edges = new ArrayList<Edge>(4);
 	
 	public void addEdge(Edge myEdge){
 		edges.add(myEdge);
